21c9d70091f2bc245083a33e04e4307fdff653ef
Shaun Keys <shaun.keys@charter.com>
2023-01-05 13:54:55 -0700

    Fix underlines

--- zellij-server/src/panes/terminal_character.rs
+++ zellij-server/src/panes/terminal_character.rs
@@ -566,9 +566,7 @@ impl Display for CharacterStyles {
                     write!(f, "\u{1b}[1m")?;
                 },
                 AnsiCode::Reset => {
-                    write!(f, "\u{1b}[22m\u{1b}[24m")?;
-                    // TODO: this cancels bold + underline, if this behaviour is indeed correct, we
-                    // need to properly handle it in the struct methods etc like dim
+                    write!(f, "\u{1b}[22m")?;
                 },
                 _ => {},
             }
